00b29f5213e59bf3c42ad3117b738017d22e99e5
Anna Rasburn <annarasburn97@gmail.com>
2017-10-22 06:05:49 +0000

    Eventful API call progress - Anna

--- eventful_api.js
+++ eventful_api.js
@@ -1,26 +1,87 @@
-var eventful = require('eventful-node');
-var client = new eventful.Client('TD6FdJXNngq5Cppt');
+//var eventful = require('eventful-node');
+//var client = new eventful.Client('TD6FdJXNngq5Cppt');
 
-client.searchEvents({ keywords: 'Marina' }, function(err, data){
- 
-  if(err){
-  
-    return console.error(err);
-  
-  }
-  
-  console.log('Recieved ' + data.search.total_items + ' events');
-  
-  console.log('Event listings: ');
-  
-  //print the title of each event 
-  for(var i in data.search.events){
-  
-    console.log(data.search.events[i].title);
-  
-  }
- 
-});
+//<html>
+
+//Javascript Sample page <br>
+
+//<script type="text/javascript" src="http://api.eventful.com/js/api">
+
+//</script>
+
+//<script type="text/javascript">
+
+//https://github.com/eventful
+
+function show_alert()
+
+{
+
+  var oArgs = {
+
+            app_key:"TD6FdJXNngq5Cppt",
+
+            id: "20218701",
+
+            page_size: 25 ,
+
+  };
+
+  EVDB.API.call("/events/get", oArgs, function(oData) {
+
+      // Note: this relies on the custom toString() methods below
+
+    });
+
+}
+
+
+
+function show_alert2()
+
+{
+
+   var oArgs = {
+
+      app_key: "YOUR APP KEY",
+
+      q: "music",
+
+      where: "Marina", 
+
+      "date": "2013061000-2015062000",
+
+      page_size: 5,
+
+      sort_order: "popularity",
+
+   };
+
+   EVDB.API.call("/events/search", oArgs, function(oData) {
+
+      // Note: this relies on the custom toString() methods below
+
+    });
+
+}
+
+//</script>
+
+
+
+//<body>
+
+//Run Sample api searches <br><br>
+
+//<input type="button" onclick="show_alert()" value="Query #1 " /> <br>
+
+//<input type="button" onclick="show_alert2()" value="Query #2 " /> <br>
+
+//</body>
+
+//</html>
+
+
 
 
 //Testing
