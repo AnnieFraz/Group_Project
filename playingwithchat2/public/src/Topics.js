7132b846e360421b9b5b6e31240b940ed837355a
Anna Rasburn <annarasburn97@gmail.com>
2017-11-26 22:31:14 +0000

    profile page

--- playingwithchat2/public/src/Topics.js
+++
@@ -1,9 +0,0 @@
-var Topics = {
-  USER_CONNECTED   : 'user-connected',
-  USER_DISCONNECTED: 'user-disconnected',
-  USER_MESSAGE     : 'user-message'
-};
-
-if (typeof module !== 'undefined') {
-  module.exports = Topics;
-}
