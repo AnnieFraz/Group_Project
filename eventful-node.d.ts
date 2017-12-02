80d848b7e9c0d77dc48da347b4fdcab9a50a603c
Alejandro R <aruvalcaba@csumb.edu>
2017-12-02 00:34:08 +0000

    Got the database set up

---
+++ eventful-node.d.ts
@@ -0,0 +1,41 @@
+declare module eventful {
+    interface searchEventOptions {
+        location: string;
+        date: string;
+        category: string;
+        within: string;
+        units: string;
+        count_only: boolean;
+        sort_order: string;
+        sort_direction: string;
+        page_size: number;
+        page_number: number;
+        image_sizes: string;
+        languages: number;
+        mature: string;
+        include: string;
+    }
+    interface searchEventResults {
+        total_items: number;
+        page_size: number;
+        page_count: number;
+        page_number: number;
+        page_items: number;
+        first_item: number;
+        last_item: number;
+        search_time: number;
+        events: any[];
+    }
+    interface categoryResult {
+        id: string;
+        name: string;
+    }
+    class Client {
+        private app_key;
+        private base_url;
+        constructor(key: string);
+        public searchEvents(options: searchEventOptions, callback: (err: any, data: searchEventResults) => void): void;
+        public listCategories(callback: (err: any, data: categoryResult[]) => void): void;
+    }
+}
+export = eventful;
---
+++ js/script.js
@@ -0,0 +1,142 @@
+var page = 0;
+
+// var keys = {
+//   tmkey: process.env.TICKETMASTER_API_KEY
+// }
+
+
+function getEvents(page) {
+ console.log("apikey " + window.TICKETMASTER_API_KEY)
+  
+  
+  $('#events-panel').show();
+  $('#attraction-panel').hide();
+
+  if (page < 0) {
+    page = 0;
+    return;
+  }
+  if (page > 0) {
+    if (page > getEvents.json.page.totalPages-1) {
+      page=0;
+      return;
+    }
+  }
+  
+  $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG&size=4&page="+page,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+          getEvents.json = json;
+  			  showEvents(json);
+  		   },
+    error: function(xhr, status, err) {
+  			  console.log(err);
+  		   }
+  });
+}
+
+function searchEvents(keyword) {
+  $('#events-panel').show();
+  $('#attraction-panel').hide();
+
+  if (page < 0) {
+    page = 0;
+    return;
+  }
+  if (page > 0) {
+    if (page > getEvents.json.page.totalPages-1) {
+      page=0;
+      return;
+    }
+  }
+  
+  $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + keyword + "&apikey=5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG&size=4&page="+page,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+          getEvents.json = json;
+  			  showEvents(json);
+  		   },
+    error: function(xhr, status, err) {
+  			  console.log(err);
+  		   }
+  });
+}
+
+function showEvents(json) {
+  var items = $('#events .list-group-item');
+  items.hide();
+  var events = json._embedded.events;
+  var item = items.first();
+  for (var i=0;i<events.length;i++) {
+    item.children('.list-group-item-heading').text(events[i].name);
+    item.children('.list-group-item-text').text(events[i].dates.start.localDate);
+    try {
+      item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
+    } catch (err) {
+      console.log(err);
+    }
+    item.show();
+    item.off("click");
+    item.click(events[i], function(eventObject) {
+      console.log(eventObject.data);
+      try {
+        getAttraction(eventObject.data._embedded.attractions[0].id);
+      } catch (err) {
+      console.log(err);
+      }
+    });
+    item=item.next();
+  }
+}
+
+$('#prev').click(function() {
+  getEvents(--page);
+});
+
+$('#next').click(function() {
+  getEvents(++page);
+});
+
+function getAttraction(id) {
+  $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/attractions/"+id+".json?apikey=ZuGAI6DrdBeDXwMKU73YqMtLwWnB1fA8",
+    async:true,
+    dataType: "json",
+    success: function(json) {
+          showAttraction(json);
+  		   },
+    error: function(xhr, status, err) {
+  			  console.log(err);
+  		   }
+  });
+}
+
+function showAttraction(json) {
+  $('#events-panel').hide();
+  $('#attraction-panel').show();
+  
+  $('#attraction-panel').click(function() {
+    getEvents(page);
+  });
+  
+  $('#attraction .list-group-item-heading').first().text(json.name);
+  $('#attraction img').first().attr('src',json.images[0].url);
+  $('#classification').text(json.classifications[0].segment.name + " - " + json.classifications[0].genre.name + " - " + json.classifications[0].subGenre.name);
+}
+
+$('#search_form').submit(function (evt) {
+    evt.preventDefault();
+    let keyword = $('#search_input').val();
+    //alert(keyword);
+    searchEvents(keyword);
+  
+});
+
+getEvents(page);
