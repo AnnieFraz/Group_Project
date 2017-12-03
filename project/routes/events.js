af6f3fa656323260bf8d0b505c99ff03c953cf0a
Andrew <druu831@gmail.com>
2017-12-03 23:28:11 +0000

    commit

--- project/routes/events.js
+++ project/routes/events.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+
+
 router.get('/', function(req, res, next) {
-  res.render('events', { title: process.env.TICKET_MASTER_API_KEY });
+  res.render('events');
 });
 
 module.exports = router;
