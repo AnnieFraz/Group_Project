0cb95d8f2116a55b0f9257a3324f2d28f7a11b3b
Andrew <druu831@gmail.com>
2017-12-07 22:26:05 +0000

    major changes to project

--- js/script.js
+++ js/script.js
@@ -31,6 +31,50 @@ function getEvents(page) {
   });
 }
 
+function searchEvents(searchFilter, searchCriteria) {
+  $('#events-panel').show();
+  $('#attraction-panel').hide();
+  
+  let date = new Date();
+  let dd = date.getDate();
+  let mm = date.getMonth() + 1;
+  let yyyy = date.getFullYear();
+  
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+  // 2017-12-06T16:37:00Z
+  let today = yyyy + '-' + mm + '-' + dd + 'T00:00:00Z';
+  console.log(today);
+  
+  if (page < 0) {
+    page = 0;
+    return;
+  }
+  if (page > 0) {
+    if (page > getEvents.json.page.totalPages - 1) {
+      page = 0;
+      return;
+    }
+  }
+    $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?" + searchFilter + "=" + searchCriteria + "&startDateTime=" + today + "&apikey=5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG&size=4&page="+page,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+      getEvents.json = json;
+      showEvents(json);
+    },
+    error: function(xhr, status, err) {
+      console.log(err);
+    }
+  });
+}
+
 function showEvents(json) {
   var items = $('#events .list-group-item');
   items.hide();
@@ -88,6 +132,7 @@ function getAttraction(id) {
 function showAttraction(json) {
   $('#events-panel').hide();
   $('#attraction-panel').show();
+  $('#profile-alert').hide();
 
   $('#card-go-back').click(function() {
     getEvents(page);
@@ -98,6 +143,13 @@ function showAttraction(json) {
     // alert(url)
     window.open(url)
   });
+  
+  $('#add-events-profile').click(function() {
+      // $('#alert-profile').show();
+      alert('this is an alert');
+  })
+  
+  
 
   $('#attraction card-title').first().text(json.name);
   $('#attraction img').first().attr('src', json.images[0].url);
@@ -105,11 +157,15 @@ function showAttraction(json) {
   $('#description'.text(json.description));
 }
 
+
 $('#search_form').submit(function (evt) {
     evt.preventDefault();
-    let keyword = $('#search_input').val();
-    //alert(keyword);
-    searchEvents(keyword);
+    let searchFilter = $("#search_filter").val();
+    // searchFilter = searchFilter.substr(3); // removes the 'by ' phrase from the filter to get key to use in url
+    alert(searchFilter);
+    let searchCriteria = $('#search_input').val();
+    alert(searchCriteria);
+    searchEvents(searchFilter ,searchCriteria);
   
 });
 
