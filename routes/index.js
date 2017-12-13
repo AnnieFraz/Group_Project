5828a57202417cd9e994517647a7b7c06790f1db
Nicholas Rosas <nrosas@csumb.edu>
2017-12-13 03:57:24 +0000

    Include mongo in router file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
 //Setting up express and a router.
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+
+var url = 'mongodb://localhost:27017/name_picker';
+
+// mongo.connect(url, function(err, db) {
+//     //assert.equal(null, err);
+//     var cursor = db.collection('users').find();
+//     cursor.forEach(function(doc, err) {
+//     //   assert.equal(null, err);
+//             console.log(doc);
+//     }, function() {
+//       db.close();
+//     });
+//     // db.close();
+//   });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
